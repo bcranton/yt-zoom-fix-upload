e6ef8f3d0abf2bc44c9aed35ceb681f70018adc0
श्रे! 𓆰𓇻𓆪 <141615291+Shreyansh511@users.noreply.github.com>
2026-05-08 22:45:12 +0530

    Alternatively, Those who don't want to install the extension and have script-injecting extensions(ex. Tampermonkey, etc..), Can Use This Script Instead
    This script disables the pinch zoom feature on YouTube's web player to enhance user experience.

---
+++ SCRIPT.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         Disable YT Player Pinch Zoom
+// @namespace    http://tampermonkey.net/
+// @version      2026-05-08
+// @description  Disables The Annoying "Video Pinch Zoom" in YouTube-Web's New Update[8-May-2026]
+// @author       bcranton
+// @match        https://www.youtube.com/
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    (() => {
+        const blockPinch = (e) => {
+            if (e.ctrlKey) e.stopImmediatePropagation();
+        };
+        window.addEventListener('wheel', blockPinch, { capture: true, passive: false });
+
+    // Belt-and-suspenders: also stop gesture events if Chrome surfaces them.
+        ['gesturestart', 'gesturechange', 'gestureend'].forEach((type) => {
+            window.addEventListener(type, (e) => e.stopImmediatePropagation(),
+                { capture: true, passive: false });
+        });
+    })();
+})();
